0f379c3afaad0bb8e770d94528058165a16b713d
Kclemen <kclemens8@hotmail.com>
2016-12-04 12:27:57 +0100

    Add files via upload

---
+++ magicmirror_skywriter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import signal
+import skywriter
+
+some_value = 5000
+
+last_airwheel = 0
+delay = 5000
+
+def to_node(type, message):
+    # convert to json and print (node helper will read from stdout)
+    try:
+        print(json.dumps({type: message}))
+    except Exception:
+        pass
+    # stdout has to be flushed manually to prevent delays in the node helper communication
+	sys.stdout.flush()
+
+to_node("status", "Skywriter started...")
+
+@skywriter.flick()
+def flick(start,finish):
+  
+  #slide music player div OFF the screen
+  if(start == "north" and finish == "south"):
+    print "gesture is north to south"
+	to_node("gesture", "northtosouth")
+    #system("irsend SEND_ONCE TV KEY_VOLUMEDOWN")
+ 
+ #slide music player div ONTO the screen
+  elif(start == "south" and finish == "north"):
+    print "gesture is south to north"
+	to_node("gesture", "southtonorth")
+   # system("irsend SEND_ONCE TV KEY_VOLUMEUP")
+  
+  #Next track
+  elif(start == "west" and finish == "east"):
+    print "Gesture is west to east"
+	to_node("gesture", "westtoeast")
+   # system("irsend SEND_ONCE Digibox KEY_CHANNELDOWN")
+  
+  #previous track
+  elif(start == "east" and finish == "west"):
+    print "Gesture is east to west"
+	to_node("gesture", "easttowest")0
+   # system("irsend SEND_ONCE Digibox KEY_CHANNELUP")
+  else:
+    print "Invalid"
+
+signal.pause()
