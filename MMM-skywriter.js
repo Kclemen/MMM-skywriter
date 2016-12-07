4337612d9fcb1668ba66f40d42446405170efb61
Kclemen <kclemens8@hotmail.com>
2016-12-07 21:12:07 +0100

    Add files via upload

---
+++ MMM-skywriter.js
@@ -0,0 +1,69 @@
+Module.register("MMM-skywriter",{
+	
+	// if gesture_up is zero swipe left or right switches between 'profiles'
+	var gesture_up = 0;
+	var gesture_right = 0;
+
+// Override socket notification handler.
+	socketNotificationReceived: function(notification, payload) {
+		if (payload == "up"){
+			if (gesture_up = 0) {
+				gesture_up = gesture_up+1;
+				if (gesture_up = 1){
+					// gesture left or right controls the popup
+					
+					MM.getModules().withClass(this.config.defaultClass).exceptWithClass(this.config.everyoneClass).enumerate(function(module) {
+						module.hide(1000, function() {
+							Log.log(module.name + ' is hidden.');
+						});
+					});
+			
+					MM.getModules().withClass(class_up_1_show).enumerate(function(module) {
+						module.show(1000, function() {
+							Log.log(module.name + ' is shown.');
+						});
+					});
+				}
+
+			}
+			//this.sendNotification("CURRENT_USER", this.current_user);
+		}
+		else if (payload == "left"){
+			if (gesture_up = 1) {
+				// Perform action in popup with left
+				Log.log('popup 1 is active - swipe left');				
+			}
+			else if (gesture_right = 0 ) {
+				// first profile is shown, left swiping does nothing
+				Log.log('first profile active - no swipe left action possible');
+			}
+			else if (gesture_right = 1) {
+				// second profile is shown, left swiping goes back to first profile
+				Log.log('2nd profile is active - left swipe to first profile');
+			}
+		}
+		else if (payload == "down") {
+			if (gesture_up = 1) {
+				// // go back to main profile page
+				Log.log('popup 1 is active - swipe down');				
+			}
+		}
+	},
+	
+	notificationReceived: function(notification, payload, sender) {
+		if (notification === 'DOM_OBJECTS_CREATED') {
+			MM.getModules().exceptWithClass("default").enumerate(function(module) {
+				module.hide(1000, function() {
+					Log.log('Module is hidden.');
+				});
+			});
+		}
+	},
+	
+	start: function() {
+		this.current_user = null;
+		this.sendSocketNotification('CONFIG', this.config);
+		Log.info('Starting module: ' + this.name);
+	}
+	
+});
---
+++ node_helper.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const NodeHelper = require('node_helper');
+const PythonShell = require('python-shell');
+var pythonStarted = false
+
+module.exports = NodeHelper.create({
+  
+	python_start: function () {
+		const self = this;
+		const pyshell = new PythonShell('modules/' + this.name + '/MMM-skywriter.py', { mode: 'json', args: [JSON.stringify(this.config)]});
+
+		pyshell.on('message', function (message) {
+      
+			if (message.hasOwnProperty('status')){
+			console.log("[" + self.name + "] " + message.status);
+			}
+			
+			if (message.hasOwnProperty('gesture')){
+			console.log("[" + self.name + "] " + message.gesture);
+			self.sendSocketNotification("gesture" + message.gesture);
+			}
+		}
+	}
+
+	// Subclass socketNotificationReceived received.
+	socketNotificationReceived: function(notification, payload) {
+		if(notification === 'CONFIG') {
+		  this.config = payload
+		  if(!pythonStarted) {
+			pythonStarted = true;
+			this.python_start();
+		  };
+		};
+	}
+	
+}
